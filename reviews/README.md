8a85270923ef3dc86192d28007945b9399f90e74
Ivan Sosa <ivandev2675@gmail.com>
2025-09-02 20:37:13 -0500

    Agrego proyecto Reviews con README e imágenes, sin node_modules

---
+++ reviews/README.md
@@ -0,0 +1,36 @@
+# Reseñas en React 
+<p align="center">
+  <img src="./image1.png" alt="Vista previa 1" width="45%"/>
+  <img src="./image2.png" alt="Vista previa 2" width="45%"/>
+  <img src="./image3.png" alt="Vista previa 2" width="45%"/>
+</p>
+
+📌 Descripción
+
+Este proyecto es un ejercicio en React que muestra una lista de reseñas de personas. El usuario puede navegar entre reseñas utilizando botones de siguiente y anterior, todo controlado con estado interno.
+
+El objetivo es reforzar conceptos como componentes, props, state, el manejo de arreglos y el uso de íconos con la librería react-icons.
+
+🛠️ Tecnologías utilizadas
+
+⚛️ React con Vite
+
+📜 JavaScript (ES6+)
+
+🎨 CSS básico para estilos
+
+🔄 Hooks: useState
+
+⭐ React Icons para mejorar la interfaz visual
+
+⚙️ Funcionalidades
+
+Renderizado dinámico de reseñas desde un archivo data.js.
+
+Botones para navegar entre reseñas (siguiente y anterior).
+
+Uso de íconos (FaChevronCircleLeft, FaChevronCircleRight, FaQuoteRight) para una interfaz más atractiva.
+
+Manejo de estado para controlar el índice de la reseña actual.
+
+Estructura modular separando el componente principal y la data.
---
+++ reviews/src/App.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import reviews from "./data";
+import { useState } from "react";
+import {FaChevronCircleLeft, FaChevronCircleRight, FaQuoteRight} from "react-icons/fa";
+
+
+const App = () => {
+
+const [index, setIndex] = useState(0);
+const {name, job, image, text} = reviews[index];
+
+const nexPerson = () => {
+setIndex((currentIndex) => {
+ const newIndex = currentIndex + 1;
+ if (newIndex > reviews.length - 1) {
+  return 0;
+ } 
+ return newIndex;
+
+})
+}
+
+
+const lesPerson = () =>{
+ setIndex((currentIndex) => {
+  const newIndex = currentIndex - 1;
+  if (newIndex < 0) {
+   return reviews.length - 1;
+  }
+  return newIndex;
+ })
+
+}
+
+
+
+
+
+
+return <main> 
+  <article className="review">
+    <div className="img-container">
+      <img src={image} alt={name} className="person-img" />
+      <span className="quote-icon">
+        <FaQuoteRight />
+      </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+
+
+
+       <div className="button-container">
+          <button className="prev-btn" onClick={lesPerson}>
+            <FaChevronCircleLeft />
+          </button>
+          <button className="next-btn" onClick={nexPerson}>
+            <FaChevronCircleRight />
+          </button>
+
+      
+
+
+
+
+      </div>
+
+
+
+
+
+  </article>
+  </main>
+
+
+};
+
+export default App;
